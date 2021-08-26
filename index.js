b5397e37e741a5f9c40e8397a410d8e5b6439b2a
Niranjan9705 <82891575+Niranjan9705@users.noreply.github.com>
2021-08-26 13:12:13 -0400

    created the server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+var http = require('http');
+
+var server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text');
+    res.end('Hello World');
+});
+
+server.listen(8080); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
+
